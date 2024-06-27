e2a9307cebac0f25b8b0a3411ba8a855235104b0
Sydney Hurst <evesperson@gmail.com>
2024-06-27 17:08:42 -0600

    team page update

--- app/Http/Controllers/BrowseController.php
+++ app/Http/Controllers/BrowseController.php
@@ -84,7 +84,7 @@ class BrowseController extends Controller
     {
         $staffRanks = RankPower::distinct()->get(['rank_id']);
         $staffRanks->push(User::where("id",Settings::get('admin_user'))->first()->rank_id);
-        $staff = User::whereIn('rank_id', $staffRanks)->get()->groupBy('rank_id');
+        $staff = User::join('ranks','users.rank_id',"=",'ranks.id')->select('users.*')->whereIn('rank_id', $staffRanks)->orderBy('sort', 'DESC')->get()->groupBy('rank_id');
         $ranks = Rank::orderBy('id')->get()->keyBy('id');
         return view('browse.team_index', [
             'staff' => $staff,
