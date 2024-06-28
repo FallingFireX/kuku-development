482af27a8de6a95f06ffa3af0bd787a7d8539cea
Sydney Hurst <evesperson@gmail.com>
2024-06-28 10:01:34 -0600

    odd bug fix maybe

--- resources/views/admin/masterlist/create_character.blade.php
+++ resources/views/admin/masterlist/create_character.blade.php
@@ -255,11 +255,6 @@
         <hr>
 
         <div class="form-group">
-            {!! Form::label('Genotype') !!}{!! add_help('This is text that will show alongside the ' . __('transformations.transformation') . ' name on the image info area. Explains why the character takes this form, how, etc. Should be pretty short.') !!}
-            {!! Form::text('genotype', ['class' => 'form-control mr-2', 'placeholder' => 'Origin Info (Optional)']) !!}
-        </div>
-
-        <div class="form-group">
             {!! Form::label('Traits') !!} @if ($isMyo)
                 {!! add_help(
                     'These traits will be listed as required traits for the slot. The user will still be able to add on more traits, but not be able to remove these. This is allowed to conflict with the rarity above; you may add traits above the character\'s specified rarity.',
