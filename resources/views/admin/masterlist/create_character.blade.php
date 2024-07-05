4f080ad7b9a7d606ffd5d5793ab8117c5085067b
Sydney Hurst <evesperson@gmail.com>
2024-07-05 13:48:29 -0600

    create chara fix

--- resources/views/admin/masterlist/create_character.blade.php
+++ resources/views/admin/masterlist/create_character.blade.php
@@ -256,7 +256,7 @@
 
         <div class="form-group" id='gender'>
             {!! Form::label('gender') !!}{!! add_help('This is text that will show alongside the ' . __('transformations.transformation') . ' name on the image info area. Explains why the character takes this form, how, etc. Should be pretty short.') !!}
-            {!! Form::select('gender', ['Rook' => 'Rook', 'Dove' => 'Dove'], null, ['class' => 'form-control']) !!}
+            {!! Form::select('gender', ['Rook' => 'Rook', 'Dove' => 'Dove'], ['class' => 'form-control']) !!}
         </div>
 
         <div class="form-group" id='genotype'>
@@ -283,7 +283,7 @@
         </div>
 
         <div class="form-group">
-            {!! Form::label('Traits') !!} @if ($isMyo)
+            {!! Form::label('Traits') !!} @if ($isMyo) 
                 {!! add_help(
                     'These traits will be listed as required traits for the slot. The user will still be able to add on more traits, but not be able to remove these. This is allowed to conflict with the rarity above; you may add traits above the character\'s specified rarity.',
                 ) !!}
