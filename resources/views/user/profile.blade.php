0a7a10c28ab4bbd47c3b13fcf6cd87b2c16b6bec
Sydney Hurst <evesperson@gmail.com>
2024-07-18 17:56:20 -0600

    section fix

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -15,6 +15,8 @@
 @section('profile-content')
     {!! breadcrumbs(['Users' => 'users', $user->name => $user->url]) !!}
     @endsection
+
+    @section('features')
     @include('widgets._awardcase_feature', ['target' => $user, 'count' => Config::get('lorekeeper.extensions.awards.user_featured'), 'float' => false])
     @include('widgets._selected_character', ['character' => $user->settings->selectedCharacter, 'user' => $user, 'fullImage' => false])
 
