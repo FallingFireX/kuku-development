e58a2a827a56d9cc130e7cfffc9636c3e15f58cf
Sydney Hurst <evesperson@gmail.com>
2024-07-18 17:40:51 -0600

    controller fix take 2

--- app/Http/Controllers/Users/AccountController.php
+++ app/Http/Controllers/Users/AccountController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Users;
 use File;
 use Settings;
 use Carbon\Carbon;
-use App\Http\Controllers\Users;
+use App\Http\Controllers\Controller;
 use App\Models\Theme;
 use App\Models\User\StaffProfile;
 use App\Models\Character\BreedingPermission;
