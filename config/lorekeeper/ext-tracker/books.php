aa22fe3c39aecee3e45b83dd747cb84ed0d8046e
Sydney Hurst <evesperson@gmail.com>
2024-07-19 15:47:31 -0600

    extensions

---
+++ config/lorekeeper/ext-tracker/books.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+        'wiki_key' => 'Books',
+        'creators' => json_encode([
+            'CH3RVB' => 'https://github.com/CH3RVB/',
+        ]),
+        'version' => '2.0.0',
+    ];
---
+++ config/lorekeeper/ext-tracker/user_name_changes.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+        'key' => 'user_username_changes',
+        'wiki_key' => 'User_Username_Changes',
+        'creators' => json_encode([
+            'AnimatedCritter' => 'https://github.com/animatedcritter/',
+        ]),
+        'version' => '1.0.0',
+    ];
