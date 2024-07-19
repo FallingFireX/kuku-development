94be7dd1d4e15f8f11887a9527f975efc1d84f1b
Sydney Hurst <evesperson@gmail.com>
2024-07-19 15:56:25 -0600

    update extensions 2

--- app/Console/Commands/UpdateExtensionTracker.php
+++ app/Console/Commands/UpdateExtensionTracker.php
@@ -54,18 +54,18 @@ class UpdateExtensionTracker extends Command {
                     'version'  => $data['version'],
                 ]);
                 $this->info('Added:   '.$key.' / Version: '.$data['version']);
-            } elseif ($extension->first()->version != $data['version']) {
-                    DB::table('site_extensions')->where('key', $key)->update([
-                        'key'      => $key,
-                        'wiki_key' => $data['wiki_key'],
-                        'creators' => $data['creators'],
-                        'version'  => $data['version'],
-                    ]);
-                    $this->info('Updated:   '.$key.' / Version: '.$data['version']);
+            // } elseif ($extension->first()->version != $data['version']) {
+            //         DB::table('site_extensions')->where('key', $key)->update([
+            //             'key'      => $key,
+            //             'wiki_key' => $data['wiki_key'],
+            //             'creators' => $data['creators'],
+            //             'version'  => $data['version'],
+            //         ]);
+            //         $this->info('Updated:   '.$key.' / Version: '.$data['version']);
                 
-            } else {
-                $this->line('Skipped: '.$key.' / Version: '.$data['version']);
-            }
+            // } else {
+            //     $this->line('Skipped: '.$key.' / Version: '.$data['version']);
+            // }
         }
 
         if (app()->runningInConsole()) {
@@ -94,4 +94,5 @@ class UpdateExtensionTracker extends Command {
 
         $this->info("\n".'All extensions are in tracker.'."\n");
     }
+}
 }
