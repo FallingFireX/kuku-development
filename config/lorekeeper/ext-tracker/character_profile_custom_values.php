f2d184cb0e64dfc69a1d43c626cd52cccd15606e
Sydney Hurst <evesperson@gmail.com>
2024-07-19 16:06:11 -0600

    crying

--- config/lorekeeper/ext-tracker/character_profile_custom_values.php
+++ config/lorekeeper/ext-tracker/character_profile_custom_values.php
@@ -1,9 +1,9 @@
 <?php
 
-// return [
-//         'wiki_key' => 'Character_Profile_Custom_Values',
-//         'creators' => json_encode([
-//             'Pure09' => 'https://github.com/deep-ci/',
-//         ]),
-//         'version' => '1.0.0',
-//     ];
+return [
+        'wiki_key' => 'Character_Profile_Custom_Values',
+        'creators' => json_encode([
+            'Pure09' => 'https://github.com/deep-ci/',
+        ]),
+        'version' => '1.0.0',
+    ];
