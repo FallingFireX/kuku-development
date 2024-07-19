e2a9c44519c13cdf9fe26bad7a31424e89a15acf
Sydney Hurst <evesperson@gmail.com>
2024-07-18 18:32:57 -0600

    dyinggg

--- resources/views/widgets/_loot_select.blade.php
+++ resources/views/widgets/_loot_select.blade.php
@@ -51,7 +51,7 @@
                             {!! Form::select('rewardable_id[]', $raffles, $loot->rewardable_id, ['class' => 'form-control raffle-select selectize', 'placeholder' => 'Select Raffle']) !!}
                         @elseif(isset($showThemes) && $showThemes && $loot->rewardable_type == 'Theme')
                             {!! Form::select('rewardable_id[]', $themes, $loot->rewardable_id, ['class' => 'form-control theme-select selectize', 'placeholder' => 'Select Theme']) !!}
-                        @elseif($showRecipes && $loot->rewardable_type == 'Recipe')
+                        @elseif(isset($showRecipes) && $showRecipes && $loot->rewardable_type == 'Recipe')
                             {!! Form::select('rewardable_id[]', $recipes, $loot->rewardable_id, ['class' => 'form-control recipe-select selectize', 'placeholder' => 'Select Recipe']) !!}
                         @endif
                     </td>
