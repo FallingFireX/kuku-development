566fd1195e1bacd0b6fe8a11dfb8f357ec02c2af
Sydney Hurst <evesperson@gmail.com>
2024-07-19 16:17:24 -0600

    extension adds

---
+++ config/lorekeeper/ext-tracker/Levels_&_Stats.php
@@ -0,0 +1,9 @@
+<?php
+
+return[
+    'wiki_key' => 'Levels_%26_Stats',
+        'creators' => json_encode([
+            'Newt' => 'https://github.com/ne-wt/',
+        ]),
+        'version' => '1.0.0',
+    ];
