6ee64a0ec0cad077d74940c919b42cd20248014d
Sydney Hurst <evesperson@gmail.com>
2024-07-25 19:57:24 -0600

    tf fixes take 2

--- resources/views/character/character.blade.php
+++ resources/views/character/character.blade.php
@@ -22,11 +22,11 @@
     @include('character._header', ['character' => $character])
 
     @if ($character->images()->where('is_valid', 1)->whereNotNull('transformation_id')->exists())
-        <div class="card-header mb-2">
+    <div class="card-header mb-2">
             <ul class="nav nav-tabs card-header-tabs">
-            @foreach ($character->images()->where('is_valid', 1)->get() as $image)
-            <li class="nav-item">
-                        <a class="nav-link form-data-button {{ $image->id == $character->image->id ? 'active' : '' }}" data-toggle="tab" role="tab" data-id="{{ $character->image->id }}">
+                @foreach ($character->images()->where('is_valid', 1)->get() as $image)
+                    <li class="nav-item">
+                        <a class="nav-link form-data-button {{ $image->id == $character->image->id ? 'active' : '' }}" data-toggle="tab" role="tab" data-id="{{ $image->id }}">
                             {{ $image->transformation_id ? $image->transformation->name : 'Main' }} {{ $image->transformation_info ? ' ('.$image->transformation_info.')' : '' }}
                         </a>
                     </li>
