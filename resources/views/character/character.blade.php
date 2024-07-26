e3b276d71e25d3ea570d6494c59d0ab6f75cfd0c
Sydney Hurst <evesperson@gmail.com>
2024-07-25 19:01:31 -0600

    tf fix 5

--- resources/views/character/character.blade.php
+++ resources/views/character/character.blade.php
@@ -26,7 +26,7 @@
             <ul class="nav nav-tabs card-header-tabs">
                 @foreach ($character->images()->where('is_valid', 1)->get() as $image)
                     <li class="nav-item">
-                        <a class="nav-link form-data-button {{ $image->id == $character->image->id ? 'active' : '' }}" data-toggle="tab" role="tab" data-id="{{ $image->id }}">
+                        <a class="nav-link form-data-button {{ $image->id == $character->image->id ? 'active' : '' }}" data-toggle="tab" role="tab" data-id="{{ $character->image->id }}">
                             {{ $character->image->transformation_id ? $character->image->transformation->name : 'Main' }} {{ $character->image->transformation_info ? ' ('.$character->image->transformation_info.')' : '' }}
                         </a>
                     </li>
