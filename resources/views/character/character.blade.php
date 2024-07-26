f0570d5dd159ce4f25a564cc3c116efeca0a6653
Sydney Hurst <evesperson@gmail.com>
2024-07-25 19:08:07 -0600

    why me

--- resources/views/character/character.blade.php
+++ resources/views/character/character.blade.php
@@ -24,10 +24,10 @@
     @if ($character->images()->where('is_valid', 1)->whereNotNull('transformation_id')->exists())
         <div class="card-header mb-2">
             <ul class="nav nav-tabs card-header-tabs">
-                @foreach ($character->images()->where('is_valid', 1)->get() as $image)
+                @foreach ($images()->where('is_valid', 1)->get() as $image)
                     <li class="nav-item">
-                        <a class="nav-link form-data-button {{ $image->id == $character->image->id ? 'active' : '' }}" data-toggle="tab" role="tab" data-id="{{ $image->character->id }}">
-                            {{ $character->image->transformation_id ? $character->image->transformation->name : 'Main' }} {{ $character->image->transformation_info ? ' ('.$character->image->transformation_info.')' : '' }}
+                        <a class="nav-link form-data-button {{ $image->id == $image->id ? 'active' : '' }}" data-toggle="tab" role="tab" data-id="{{ $image->character->id }}">
+                            {{ $image->transformation_id ? $image->transformation->name : 'Main' }} {{ $image->transformation_info ? ' ('.$image->transformation_info.')' : '' }}
                         </a>
                     </li>
                 @endforeach
