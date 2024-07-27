12d6e10248ff7877b0c714466dc1669f9f226102
Sydney Hurst <evesperson@gmail.com>
2024-07-26 21:07:10 -0600

    element style change

--- app/Models/Element/Typing.php
+++ app/Models/Element/Typing.php
@@ -75,10 +75,10 @@ class Typing extends Model {
         // get the displayName attribute from each element
         $elements = $this->elements()->map(function ($element) {
             // check if first in loop
-            return '<a href="'.$element->idUrl.'"><span class="badge '. ($element->id == $this->elements()->first()->id ? '' : 'ml-1')
-            .'" style="color: white; background-color: '.$element->colour.';">'.$element->name.'</span></a>';
+            return '<a href="'.$element->idUrl.'"><span class="display-trait '. ($element->id == $this->elements()->first()->id ? '' : 'ml-1');
+            //.'" style="color: white; background-color: '.$element->colour.';">'.$element->name.'</span></a>';
         });
 
-        return implode(' ', $elements->toArray());
+        return implode(' ', $elements->toArray()); 
     }
 }
