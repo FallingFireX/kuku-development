0d4b36590e65cbe4b29066c330ec2157d495a83b
Sydney Hurst <evesperson@gmail.com>
2024-07-27 20:52:28 -0600

    forgot to save

--- resources/views/character/character.blade.php
+++ resources/views/character/character.blade.php
@@ -117,11 +117,11 @@
     </div>
     <br>
     <p></p>
-        @if (count($pet->image->character->pets))
+        @if (count($image->character->pets))
                     <div class="row justify-content-center text-center">
                         {{-- get one random pet --}}
                         @php
-                            $pets = $pet->image->character->pets()->orderBy('sort', 'DESC')->limit(config('lorekeeper.pets.display_pet_count'))->get();
+                            $pets = $image->character->pets()->orderBy('sort', 'DESC')->limit(config('lorekeeper.pets.display_pet_count'))->get();
                         @endphp
                         @foreach ($pets as $pet)
                             @if (config('lorekeeper.pets.pet_bonding_enabled'))
