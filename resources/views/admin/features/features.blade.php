70b30dbdc09e1efd9453a38a20add43d5354a87e
Sydney Hurst <evesperson@gmail.com>
2024-07-29 11:52:26 -0600

    non visible feature fix

--- resources/views/admin/features/features.blade.php
+++ resources/views/admin/features/features.blade.php
@@ -73,9 +73,9 @@
                         <div class="row flex-wrap">
                             <div class="col-12 col-md-3">
                                 <div class="logs-table-cell">
-                                    @if (!$feature->is_visible)
+                                    <!-- @if (!$feature->is_visible)
                                         <i class="fas fa-eye-slash mr-1"></i>
-                                    @endif
+                                    @endif -->
                                     {{ $feature->name }}
                                 </div>
                             </div>
