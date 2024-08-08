1fd7b5dc31e58a066ab7293c45fc3fe0a10f4458
Sydney Hurst <evesperson@gmail.com>
2024-08-08 17:22:04 -0600

    screaming crying

--- resources/views/admin/grants/pets.blade.php
+++ resources/views/admin/grants/pets.blade.php
@@ -55,15 +55,13 @@
 
     {!! Form::close() !!}
 
-    <div id="petList">
-            <div class="pet-row d-flex mb-2">
-                {!! Form::select('pet_ids[]', $pets, null, ['class' => 'form-control mr-2 pet-select', 'placeholder' => 'Select Pet']) !!}
-                {!! Form::text('quantities[]', 1, ['class' => 'form-control mr-2', 'placeholder' => 'Quantity']) !!}
-                {!! Form::select('variant[]', ['none' => 'No Variant', 'randomize' => 'Randomize Variant'], null, ['class' => 'form-control mr-2 variant-select']) !!}
-                {!! Form::select('evolution[]', ['none' => 'No Evolution', 'randomize' => 'Randomize Evolution'], null, ['class' => 'form-control mr-2 evolution-select']) !!}
-                <a href="#" class="remove-pet btn btn-danger mb-2 disabled">×</a>
-            </div>
-        </div>
+    <div class="pet-row hide mb-2">
+        {!! Form::select('pet_ids[]', $pets, null, ['class' => 'form-control mr-2 pet-select', 'placeholder' => 'Select Pet']) !!}
+        {!! Form::text('quantities[]', 1, ['class' => 'form-control mr-2', 'placeholder' => 'Quantity']) !!}
+        {!! Form::select('variant[]', ['none' => 'No Variant', 'randomize' => 'Randomize Variant'], null, ['class' => 'form-control mr-2 variant-select']) !!}
+        {!! Form::select('evolution[]', ['none' => 'No Evolution', 'randomize' => 'Randomize Evolution'], null, ['class' => 'form-control mr-2 evolution-select']) !!}
+        <a href="#" class="remove-pet btn btn-danger mb-2">×</a>
+    </div>
 
     <script>
         $(document).ready(function() {
