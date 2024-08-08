f458f08e215826fceb87cf10a3a535bb13501ecd
Sydney Hurst <evesperson@gmail.com>
2024-08-08 17:11:49 -0600

    yunw

--- resources/views/admin/grants/pets.blade.php
+++ resources/views/admin/grants/pets.blade.php
@@ -55,15 +55,15 @@
 
     {!! Form::close() !!}
 
-<div class="pet-select">
-    <div class="pet-row hide mb-2">
-        {!! Form::select('pet_ids[]', $pets, null, ['class' => 'form-control mr-2 pet-select', 'placeholder' => 'Select Pet']) !!}
-        {!! Form::text('quantities[]', 1, ['class' => 'form-control mr-2', 'placeholder' => 'Quantity']) !!}
-        {!! Form::select('variant[]', ['none' => 'No Variant', 'randomize' => 'Randomize Variant'], null, ['class' => 'form-control mr-2 variant-select']) !!}
-        {!! Form::select('evolution[]', ['none' => 'No Evolution', 'randomize' => 'Randomize Evolution'], null, ['class' => 'form-control mr-2 evolution-select']) !!}
-        <a href="#" class="remove-pet btn btn-danger mb-2">×</a>
-    </div>
-</div>
+    <div id="pet-row">
+            <div class="d-flex mb-2">
+                {!! Form::select('pet_ids[]', $pets, null, ['class' => 'form-control mr-2 default pet-select', 'placeholder' => 'Select Pet']) !!}
+                {!! Form::text('quantities[]', 1, ['class' => 'form-control mr-2', 'placeholder' => 'Quantity']) !!}
+                {!! Form::select('variant[]', ['none' => 'No Variant', 'randomize' => 'Randomize Variant'], null, ['class' => 'form-control mr-2 variant-select']) !!}
+                {!! Form::select('evolution[]', ['none' => 'No Evolution', 'randomize' => 'Randomize Evolution'], null, ['class' => 'form-control mr-2 evolution-select']) !!}
+                <a href="#" class="remove-pet btn btn-danger mb-2 disabled">×</a>
+            </div>
+        </div>
 
     <script>
         $(document).ready(function() {
@@ -71,7 +71,6 @@
                 maxPets: 10
             });
             $('.default.pet-select').selectize();
-            $('.pet-select').selectize();
             $('#add-pet').on('click', function(e) {
                 e.preventDefault();
                 addPetRow();
