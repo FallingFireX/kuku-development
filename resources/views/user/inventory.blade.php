28ab04bfc9338ea9c10f480a7eadc3ca39782e6a
Sydney Hurst <evesperson@gmail.com>
2024-08-07 21:28:57 -0600

    div fix

--- resources/views/user/inventory.blade.php
+++ resources/views/user/inventory.blade.php
@@ -49,6 +49,7 @@
                             </div>
                         </div>
                     @endforeach
+                </div>
             @endforeach
         </div>
     @endforeach
