c6d5c6814d7bf98f62e6acd76ef2e430fff4987a
Sydney Hurst <evesperson@gmail.com>
2024-08-09 09:56:47 -0600

    yoink from item service

--- app/Services/PetService.php
+++ app/Services/PetService.php
@@ -241,6 +241,7 @@ class PetService extends Service {
             $image = null;
             if (isset($data['image']) && $data['image']) {
                 $data['has_image'] = 1;
+                $data['hash'] = randomString(10);
                 $image = $data['image'];
                 unset($data['image']);
             }
