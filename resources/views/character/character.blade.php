60843edafb1fd4112ecdc71ffe2885194ab9d487
Sydney Hurst <evesperson@gmail.com>
2024-08-09 20:12:31 -0600

    can i cheese this

--- resources/views/character/character.blade.php
+++ resources/views/character/character.blade.php
@@ -91,10 +91,12 @@
         @include('character._image_info', ['image' => $character->image])
     </div>
         
+    <div class="my-3 card {{ $float ? 'float' : '' }}"><div class="row justify-content-center align-items-center">
     @include('widgets._awardcase_feature', ['target' => $character, 'count' => Config::get('lorekeeper.extensions.awards.character_featured'), 'float' => true])
     <div class="ml-auto float-right mr-3">
             <a href="{{ $character->slug . '/'.__('awards.awardcase') }}" class="btn btn-outline-info btn-sm">View All</a>
-        </div>        
+        </div>  
+    </div>      
     
         
     {{-- Info --}}
