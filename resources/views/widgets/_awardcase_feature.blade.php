7cb78e59e5b5e0315b8e4c6d3b1953e4300ac398
Sydney Hurst <evesperson@gmail.com>
2024-08-09 20:54:36 -0600

    spacing changes

--- resources/views/widgets/_awardcase_feature.blade.php
+++ resources/views/widgets/_awardcase_feature.blade.php
@@ -1,7 +1,7 @@
 @if($target->awards->where('is_featured',1)->where('pivot.count','>',0)->count() && $count)
     <div class="row justify-content-center align-items-center">
         @foreach($target->awards->where('is_featured',1)->where('pivot.count','>',0)->unique()->take($count) as $award)
-            <div class="text-center mb-1 px-1 my-4">
+            <div class="text-center px-2 my-2">
                 <a href="{{$award->idUrl}}" class="alert"><img src="{{ $award->imageUrl }}" alt="{{ $award->name }}" data-toggle="tooltip" data-title="{{ $award->name }}" width="80%"/></a>
             </div>
         @endforeach
