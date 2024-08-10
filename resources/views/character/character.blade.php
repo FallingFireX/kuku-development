db367a02d7c39c1a11802b9cc11d681a10c89948
Sydney Hurst <evesperson@gmail.com>
2024-08-09 18:57:27 -0600

    chunk

--- resources/views/character/character.blade.php
+++ resources/views/character/character.blade.php
@@ -101,13 +101,7 @@
                 <div class="card-body">
                 @foreach($awards as $categoryId=>$categoryAwards)
                     <div class="card mb-3 awards-category">
-                        <h5 class="card-header awards-header">
-                            {!! isset($categories[$categoryId]) ? '<a href="'.$categories[$categoryId]->searchUrl.'">'.$categories[$categoryId]->name.'</a>' : 'Miscellaneous' !!}
-                        </h5>
-                        <div class="card-body awards-body">
-                            @foreach($categoryAwards->chunk(4) as $chunk)
-                                <div class="row mb-3">
-                                    @foreach($chunk as $awardId=>$stack)
+                                    @foreach($award as $awardId=>$stack)
                                         <div class="col-sm-3 col-6 text-center awards-award" data-id="{{ $stack->first()->pivot->id }}" data-name="{{ $character->name ? $character->name : $character->slug }}'s {{ $stack->first()->name }}">
                                             <div class="mb-1">
                                                 <a href="#" class="awards-stack {{ $stack->first()->is_featured ? 'alert alert-secondary' : '' }}"><img src="{{ $stack->first()->imageUrl }}" alt="{{ $stack->first()->name }}" class="mw-100"/></a>
@@ -121,9 +115,7 @@
                             @endforeach
                         </div>
                     </div>
-                @endforeach
-                </div>
-            </div>
+            
     
         
     {{-- Info --}}
