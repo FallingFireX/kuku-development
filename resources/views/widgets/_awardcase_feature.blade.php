d555847ab642bb4170bae11b285778238b637eab
Sydney Hurst <evesperson@gmail.com>
2024-08-09 20:44:09 -0600

    size edit

--- resources/views/widgets/_awardcase_feature.blade.php
+++ resources/views/widgets/_awardcase_feature.blade.php
@@ -2,7 +2,7 @@
     <div class="row justify-content-center align-items-center">
         @foreach($target->awards->where('is_featured',1)->where('pivot.count','>',0)->unique()->take($count) as $award)
             <div class="text-center mb-1 px-1 my-4">
-                <a href="{{$award->idUrl}}" class="alert"><img src="{{ $award->imageUrl }}" alt="{{ $award->name }}" data-toggle="tooltip" data-title="{{ $award->name }}"/></a>
+                <a href="{{$award->idUrl}}" class="alert"><img src="{{ $award->imageUrl }}" alt="{{ $award->name }}" data-toggle="tooltip" data-title="{{ $award->name }}" width="80%"/></a>
             </div>
         @endforeach
         
