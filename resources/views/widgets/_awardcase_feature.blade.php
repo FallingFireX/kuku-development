dd23fc9aa60fc9eddad0479e996a0e5c375df365
Sydney Hurst <evesperson@gmail.com>
2024-08-09 19:54:01 -0600

    variable is being ass

--- resources/views/widgets/_awardcase_feature.blade.php
+++ resources/views/widgets/_awardcase_feature.blade.php
@@ -1,7 +1,8 @@
-$character = Character::find(character_id);
-
 @if($target->awards->where('is_featured',1)->where('pivot.count','>',0)->count() && $count)
     <div class="my-3 card {{ $float ? 'float' : '' }}"><div class="row justify-content-center align-items-center">
+    @php
+                        $character = \App\Models\Character\Character::orderBy('name')->pluck('name', 'id');
+                    @endphp
         @foreach($target->awards->where('is_featured',1)->where('pivot.count','>',0)->unique()->take($count) as $award)
             <div class="text-center mb-1 px-1">
                 <a href="{{$award->idUrl}}" class="alert"><img src="{{ $award->imageUrl }}" alt="{{ $award->name }}" data-toggle="tooltip" data-title="{{ $award->name }}"/></a>
