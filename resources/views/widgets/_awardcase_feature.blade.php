1ac000f1a834c3a8b9f07e6b35efa17227a44558
Sydney Hurst <evesperson@gmail.com>
2024-08-14 08:49:49 -0600

    award display fix

--- resources/views/widgets/_awardcase_feature.blade.php
+++ resources/views/widgets/_awardcase_feature.blade.php
@@ -1,8 +1,8 @@
 @if($target->awards->where('is_featured',1)->where('pivot.count','>',0)->count() && $count)
     <div class="row justify-content-center align-items-center">
         @foreach($target->awards->where('is_featured',1)->where('pivot.count','>',0)->unique()->take($count) as $award)
-            <div class="text-center px-2 my-2">
-                <a href="{{$award->idUrl}}" class="alert"><img src="{{ $award->imageUrl }}" alt="{{ $award->name }}" data-toggle="tooltip" data-title="{{ $award->name }}" width="80%"/></a>
+            <div class="text-center my-2">
+                <a href="{{$award->idUrl}}" class="alert"><img src="{{ $award->imageUrl }}" alt="{{ $award->name }}" data-toggle="tooltip" data-title="{{ $award->name }}" width="55%"/></a>
             </div>
         @endforeach
         
