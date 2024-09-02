9bf9dd2f84f0b66fb895d613b02b318ba467da46
Sydney Hurst <evesperson@gmail.com>
2024-09-01 22:06:20 -0600

    welcome page test

--- resources/views/pages/_logged_out.blade.php
+++ resources/views/pages/_logged_out.blade.php
@@ -1,2 +1,16 @@
 <h1>{{ config('lorekeeper.settings.site_name', 'Lorekeeper') }}</h1>
-{!! $about->parsed_text !!}
+<div class="row justify-content-center">
+    <div class="col-md-4">
+        <div class="card mb-4">
+            <div class="card-body text-center">
+                {!! $about->parsed_text !!}
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="card mb-4">
+            <div class="card-body text-center">
+@include('widgets._news', ['textPreview' => true])
+            </div>
+        </div>
+    </div>
