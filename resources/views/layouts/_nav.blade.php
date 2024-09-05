0ad5c0e4dcbdd3f689eef41668c220608bc096e7
Sydney Hurst <evesperson@gmail.com>
2024-09-04 20:58:53 -0600

    nav edits

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -1,9 +1,15 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark" id="headerNav">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark col-lg-8" id="headerNav" style="margin:auto">
     
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{ url('/') }}">
+        <!-- <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('lorekeeper.settings.site_name', 'Lorekeeper') }}
-        </a>
+        </a> -->
+        <span class="button">
+            <a href="{{ url('/') }}">
+                <span class="text">HOME</span>
+                <span class="icon"><</span>
+            </a>
+        </span>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
