ab3d90cba51d31bd3680e46f2769d82e9f4e8fd5
Sydney Hurst <evesperson@gmail.com>
2024-09-11 00:00:19 -0600

    add create character award

---
+++ config/lorekeeper/staff-reward-actions/upload_design.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    // Name and description shown in the admin panel
+    'name'        => 'Uploading Designs',
+    'description' => 'Uploading designs from DA',
+    'actions'     => [
+        // Actions, as reported to admin action logging,
+        // that should use this key's reward value
+        'Created Character',
+    ],
+];
