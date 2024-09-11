582fef0106544910fdfefc11f759a86166544afc
Sydney Hurst <evesperson@gmail.com>
2024-09-11 00:14:57 -0600

    tabs in shop

--- resources/views/shops/shop.blade.php
+++ resources/views/shops/shop.blade.php
@@ -46,39 +46,41 @@
                         $visible = '<i class="fas fa-eye-slash mr-1"></i>';
                     }
                 @endphp
-                <div class="card mb-3 inventory-category">
-                    <h5 class="card-header inventory-header">
-                        {!! isset($categories[$categoryId]) ? '<a href="' . $categories[$categoryId]->searchUrl . '">' . $visible . $categories[$categoryId]->name . '</a>' : 'Miscellaneous' !!}
-                    </h5>
-                    <div class="card-body inventory-body">
-                        @foreach ($categoryItems->chunk(4) as $chunk)
-                            <div class="row mb-3">
-                                @foreach ($chunk as $item)
-                                    <div class="col-sm-3 col-6 text-center inventory-item" data-id="{{ $item->pivot->id }}">
-                                        <div class="mb-1">
-                                            <a href="#" class="inventory-stack"><img src="{{ $item->imageUrl }}" alt="{{ $item->name }}" /></a>
-                                        </div>
-                                        <div>
-                                            <a href="#" class="inventory-stack inventory-stack-name"><strong>{{ $item->name }}</strong></a>
-                                            <div><strong>Cost: </strong> {!! $currencies[$item->pivot->currency_id]->display((int) $item->pivot->cost) !!}</div>
-                                            @if ($item->pivot->is_limited_stock)
-                                                <div>Stock: {{ $item->pivot->quantity }}</div>
-                                            @endif
-                                            @if ($item->pivot->purchase_limit)
-                                                <div class="text-danger">Max {{ $item->pivot->purchase_limit }} @if ($item->pivot->purchase_limit_timeframe !== 'lifetime')
-                                                        {{ $item->pivot->purchase_limit_timeframe }}
-                                                    @endif per user</div>
-                                            @endif
-                                            @if ($item->pivot->disallow_transfer)
-                                                <div class="text-danger">Cannot be transferred after purchase</div>
-                                            @endif
-                                        </div>
-                                    </div>
-                                @endforeach
+                <ul class="nav nav-tabs card-header-tabs">
+        @foreach ($items as $categoryId => $categoryItems)
+            <li class="nav-item">
+                <a class="nav-link {{ $loop->first ? 'active' : '' }}" id="categoryTab-{{ isset($categories[$categoryId]) ? $categoryId : 'misc' }}" data-toggle="tab" href="#category-{{ isset($categories[$categoryId]) ? $categoryId : 'misc' }}"
+                    role="tab">
+                    {!! isset($categories[$categoryId]) ? $categories[$categoryId]->name : 'Miscellaneous' !!}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+</div>
+<div class="card-body tab-content">
+    @foreach ($items as $categoryId => $categoryItems)
+        <div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}" id="category-{{ isset($categories[$categoryId]) ? $categoryId : 'misc' }}">
+            @foreach ($categoryItems->chunk(4) as $chunk)
+                <div class="row mb-3">
+                    @foreach ($chunk as $itemId => $stack)
+                        <div class="col-sm-3 col-6 text-center inventory-item" data-id="{{ $stack->first()->pivot->id }}" data-name="{{ $user->name }}'s {{ $stack->first()->name }}">
+                            @if ($stack->first()->has_image)
+                                <div class="mb-1">
+                                    <a href="#" class="inventory-stack">
+                                        <img src="{{ $stack->first()->imageUrl }}" style="height: 125px;" alt="{{ $stack->first()->name }}" />
+                                    </a>
+                                </div>
+                            @endif
+                            <div>
+                                <a href="#" class="inventory-stack inventory-stack-name">{{ $stack->first()->name }} x{{ $stack->sum('pivot.count') }}</a>
                             </div>
-                        @endforeach
-                    </div>
+                        </div>
+                    @endforeach
                 </div>
+            @endforeach
+        </div>
+    @endforeach
+</div>
             @endforeach
         @endif
     @endforeach
