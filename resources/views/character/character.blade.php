0ec94f2802a6b39ec2bc82eaa65b0a3d8ad4cc99
Sydney Hurst <evesperson@gmail.com>
2024-09-25 12:09:11 -0600

    fix chara blade

--- resources/views/character/character.blade.php
+++ resources/views/character/character.blade.php
@@ -36,18 +36,6 @@
         </div>
 @endif
 
-    <!-- {{-- Main Image --}}
-    <div class="row mb-3" id="main-tab">
-        <div class="col-md-7">
-            <div class="text-center">
-                <a href="{{ $character->image->canViewFull(Auth::check() ? Auth::user() : null) && file_exists(public_path($character->image->imageDirectory . '/' . $character->image->fullsizeFileName)) ? $character->image->fullsizeUrl : $character->image->imageUrl }}"
-                    data-lightbox="entry" data-title="{{ $character->fullName }}">
-                    <img src="{{ $character->image->canViewFull(Auth::check() ? Auth::user() : null) && file_exists(public_path($character->image->imageDirectory . '/' . $character->image->fullsizeFileName)) ? $character->image->fullsizeUrl : $character->image->imageUrl }}"
-                        class="image" alt="{{ $character->fullName }}" />
-                </a>
-            </div>
-            @if ($character->image->canViewFull(Auth::check() ? Auth::user() : null) && file_exists(public_path($character->image->imageDirectory . '/' . $character->image->fullsizeFileName)))
-                <div class="text-right">You are viewing the full-size image. <a href="{{ $character->image->imageUrl }}">View watermarked image</a>?</div> -->
 @if(Auth::check() && (Auth::user()->settings->warning_visibility < 2) && isset($character->character_warning) || isset($character->character_warning) && !Auth::check())
 <div id="warning" class="alert alert-danger" style="text-align:center;">
     <span style="float:right;"><a href="#" data-id="{{ $character->character_warning }}" onclick="changeStyle()"><i class="fas fa-times" aria-hidden="true"></i></a></span>
