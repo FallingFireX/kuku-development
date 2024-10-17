aadabbca9be77d26ce50646ab47b301fb0109d35
Sydney Hurst <evesperson@gmail.com>
2024-10-17 09:50:09 -0600

    fix

--- resources/views/user/_characters.blade.php
+++ resources/views/user/_characters.blade.php
@@ -9,7 +9,7 @@
             
         <div class="card-body inventory-body">
             <div class="row mb-2">
-                @foreach ($characters as $character)
+                @foreach ($group as $character)
                     <div class="col-md-3 col-6 text-center mb-2">
                         @if(Auth::check() && (Auth::user()->settings->warning_visibility == 0) && isset($character->character_warning) || isset($character->character_warning) && !Auth::check())
                         <a href="{{ $character->url }}"><img class="img-thumbnail" src="{{ asset('/images/content_warning.png') }}" alt="Content Warning"/></a>
@@ -44,17 +44,3 @@
 @else
     <p>No {{ $myo ? 'MYO slots' : 'characters' }} found.</p>
 @endif
-
-<!-- <div>
-                            <a href="{{ $character->url }}"><img src="{{ $character->image->thumbnailUrl }}" class="img-thumbnail" alt="Thumbnail for {{ $character->fullName }}" /></a>
-                        </div>
-                        <div class="mt-1">
-                            <a href="{{ $character->url }}" class="h5 mb-0">
-                                @if (!$character->is_visible)
-                                    <i class="fas fa-eye-slash"></i>
-                                @endif {{ Illuminate\Support\Str::limit($character->fullName, 20, $end = '...') }}
-                            </a>
-                        </div>
-                        <div class="small">
-                            {!! $character->image->species_id ? $character->image->species->displayName : 'No Species' !!} ・ {!! $character->image->rarity_id ? $character->image->rarity->displayName : 'No Rarity' !!}
-                        </div> -->
