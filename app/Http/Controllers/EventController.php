b4d6a233db7992b15ecf41e140f5f0a59be6b5e3
Sydney Hurst <evesperson@gmail.com>
2025-03-31 10:06:49 -0600

    file hosting method

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -26,34 +26,12 @@ class EventController extends Controller
     $userTheme = auth()->user()->theme;
     $page = SitePage::where('key', 'fod-2025')->first();
 
-    // External URL or default theme
+    // Use the on-site file manager CSS path
     $themeUrl = ($page && $page->key === 'fod-2025') 
-        ? 'https://www.kukuri-arpg.com/themes/2.css'   // Remote theme
+        ? 'https://www.kukuri-arpg.com/files/Festival2.css'   // Local file manager theme
         : $userTheme;
 
-    // Handle external CSS by downloading it locally
-    if (filter_var($themeUrl, FILTER_VALIDATE_URL)) {
-        // Define local path on the live server
-        $localPath = public_path('css/external-theme.css');
-
-        // Download only if the file doesn't exist or is outdated
-        if (!File::exists($localPath) || now()->diffInMinutes(File::lastModified($localPath)) > 60) {
-            try {
-                $response = Http::get($themeUrl);
-
-                if ($response->successful()) {
-                    File::put($localPath, $response->body());
-                }
-            } catch (\Exception $e) {
-                \Log::error("Failed to download theme: " . $e->getMessage());
-            }
-        }
-
-        // Use the local file path in the view
-        $themeUrl = 'css/external-theme.css';
-    }
-
-    // Pass the local or original theme path
+    // Pass the theme object to match the theme manager structure
     $theme = (object) [
         'cssUrl' => $themeUrl,
         'prioritize_css' => false,  
@@ -65,4 +43,5 @@ class EventController extends Controller
         'theme' => $theme
     ]);
 }
+
 }
