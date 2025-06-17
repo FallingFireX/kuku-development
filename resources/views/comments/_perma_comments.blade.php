2b8b4defdc7168c346a5322e3fc13ae9be57910e
Sydney Hurst <evesperson@gmail.com>
2025-06-17 15:30:36 -0600

    fix comments

--- resources/views/comments/_perma_comments.blade.php
+++ resources/views/comments/_perma_comments.blade.php
@@ -2,47 +2,12 @@
 @php
     $markdown->setSafeMode(true);
 @endphp
-@if(isset($reply) && $reply === true)
-  <div id="comment-{{ $comment->getKey() }}" class="comment_replies border-left col-12 column mw-100 pr-0 pt-4" style="flex-basis: 100%;">
-@else
-  <div id="comment-{{ $comment->getKey() }}"  class="pt-4" style="flex-basis: 100%;">
-@endif
-<div class="media-body row mw-100 mx-0" style="flex:1;flex-wrap:wrap;">
-    <div class="d-none d-md-block">
-        {!!  $comment->commenter->userBorder() !!}
-    </div>
-    <div class="d-block" style="flex:1">
-        <div class="row mx-0 px-0 align-items-md-end">
-            <h5 class="mt-0 mb-1 col mx-0 px-0">
-                {!! $comment->commenter->commentDisplayName !!} @if($comment->commenter->isStaff == true)<small class="text-muted">Staff Member</small>@endif
-            </h5>
-            @if($comment->is_featured)<div class="ml-1 text-muted text-right col-6 mx-0 pr-1"><small class="text-success">Featured by Owner</small></div> @endif
-        </div>
-        <div class="border p-3 rounded {{ $limit == 0 ? 'shadow-sm border-info' : '' }} {{ ($comment->is_featured && ($limit != 0)) ? 'border-success' : '' }} "><p>{!! nl2br($markdown->line($comment->comment)) !!} </p>
-        <p class="border-top pt-1 text-right mb-0">
-            <small class="text-muted">{!! $comment->created_at !!}
-            @if($comment->created_at != $comment->updated_at) 
-                <span class="text-muted border-left mx-1 px-1">(Edited {!! ($comment->updated_at) !!})</span>
-            @endif
-            </small>
-            <a href="{{ url('comment/').'/'.$comment->id }}"><i class="fas fa-link ml-1" style="opacity: 50%;"></i></a>
-        </p>
-    </div>
-    @if(Auth::check())
-        <div class="my-1">
-            @can('reply-to-comment', $comment)
-                <button data-toggle="modal" data-target="#reply-modal-{{ $comment->getKey() }}" class="btn btn-sm px-3 py-2 px-sm-2 py-sm-1  btn-faded text-uppercase"><i class="fas fa-comment"></i><span class="ml-2 d-none d-sm-inline-block">Reply</span></button>
-            @endcan
-            @can('edit-comment', $comment)
-                <button data-toggle="modal" data-target="#comment-modal-{{ $comment->getKey() }}" class="btn btn-sm px-3 py-2 px-sm-2 py-sm-1  btn-faded text-uppercase"><i class="fas fa-edit"></i><span class="ml-2 d-none d-sm-inline-block">Edit</span></button>
-            @endcan
-            @if((Auth::user()->id == $comment->commentable_id) || Auth::user()->isStaff)
-                <button data-toggle="modal" data-target="#feature-modal-{{ $comment->getKey() }}" class="btn btn-sm px-3 py-2 px-sm-2 py-sm-1  btn-faded text-success text-uppercase"><i class="fas fa-star"></i><span class="ml-2 d-none d-sm-inline-block">{{$comment->is_featured ? 'Unf' : 'F' }}eature Comment</span></button>
-            @endif
-            @can('delete-comment', $comment)
-                <button data-toggle="modal" data-target="#delete-modal-{{ $comment->getKey() }}" class="btn btn-sm px-3 py-2 px-sm-2 py-sm-1 btn-outline-danger text-uppercase"><i class="fas fa-minus-circle"></i><span class="ml-2 d-none d-sm-inline-block">Delete</span></button>
-            @endcan
-        </div>
+
+@if ($comment->deleted_at == null)
+    @if (isset($reply) && $reply === true)
+        <div id="comment-{{ $comment->getKey() }}" class="comment_replies border-left col-12 column mw-100 pr-0 pt-4" style="flex-basis: 100%;">
+        @else
+            <div id="comment-{{ $comment->getKey() }}" class="pt-4" style="flex-basis: 100%;">
     @endif
     <div class="media-body row mw-100 mx-0" style="flex:1;flex-wrap:wrap;">
         <div class="d-none d-md-block">
@@ -60,7 +25,7 @@
                 @endif
             </div>
             <div
-                class="comment border p-3 rounded {{ $limit == 0 ? 'shadow-sm border-info' : '' }} {{ $comment->is_featured && $limit != 0 ? 'border-success' : '' }} {{ $comment->likes()->where('is_like', 1)->count() - $comment->likes()->where('is_like', 0)->count() < 0 ? 'bg-light bg-gradient' : '' }}">
+                class="comment border p-3 rounded {{ $limit == 0 ? 'shadow-sm border-info' : '' }} {{ $comment->is_featured && $limit != 0 ? 'border-success' : '' }} {{ $comment->likes()->where('is_like', 1)->count() -$comment->likes()->where('is_like', 0)->count() <0? 'bg-light bg-gradient': '' }}">
                 <p>
                     {!! config('lorekeeper.settings.wysiwyg_comments') ? $comment->comment : '<p>' . nl2br($markdown->line(strip_tags($comment->comment))) . '</p>' !!}
                 </p>
@@ -84,8 +49,8 @@
         </div>
 
         {{-- add a limit check so if limit is reached but replies are still presnt to display a button with current amount of replies
-        use child function
-        url should be equal to the last replies permalink (e.g reply 5) --}}
+use child function
+url should be equal to the last replies permalink (e.g reply 5) --}}
 
 
         {{-- Recursion for children --}}
@@ -109,8 +74,8 @@
         @if ($depth == 0)
             {!! $children->render() !!}
         @endif
-        </div>
     </div>
+</div>
 </div>
 @else
 @if (isset($reply) && $reply === true)
@@ -118,45 +83,46 @@
     @else
         <div id="comment-{{ $comment->getKey() }}" class="pt-4" style="flex-basis: 100%;">
 @endif
-    <div class="media-body row mw-100 mx-0" style="flex:1;flex-wrap:wrap;">
-        <div class="d-none d-md-block">
-            <img class="mr-3 mt-2" src="/images/avatars/default.jpg" style="width:70px; height:70px; border-radius:50%;" alt="Default Avatar">
-        </div>
-        <div class="d-block bg-light" style="flex:1">
-            <div class="border p-3 rounded {{ $limit == 0 ? 'shadow-sm border-info' : '' }}">
-                <p>Comment deleted </p>
-                <p class="border-top pt-1 text-right mb-0">
-                    <small class="text-muted">{!! $comment->created_at !!}
-                        @if ($comment->created_at != $comment->deleted_at)
-                            <span class="text-muted border-left mx-1 px-1">(Deleted {!! $comment->deleted_at !!})</span>
-                        @endif
-                    </small>
-                    <a href="{{ url('comment/') . '/' . $comment->id }}"><i class="fas fa-link ml-1" style="opacity: 50%;"></i></a>
-                </p>
-            </div>
-        </div>
-
-        {{-- Recursion for children --}}
-        <div class="w-100 mw-100">
-            @php $children = $depth == 0 ? $comment->children->sortByDesc('created_at')->paginate(5) : $comment->children->sortByDesc('created_at') @endphp
-            @foreach ($children as $reply)
-                @php $limit++; @endphp
-
-                @if ($limit >= 5 && $depth >= 1)
-                    <a href="{{ url('comment/') . '/' . $comment->id }}"><span class="btn btn-secondary w-100">See More Replies</span></a>
-                @break
-            @endif
-
-            @include('comments._perma_comments', [
-                'comment' => $reply,
-                'reply' => true,
-                'limit' => $limit,
-                'depth' => $depth + 1,
-            ])
-        @endforeach
-        @if ($depth == 0)
-            {!! $children->render() !!}
-        @endif
+<div class="media-body row mw-100 mx-0" style="flex:1;flex-wrap:wrap;">
+    <div class="d-none d-md-block">
+        <img class="mr-3 mt-2" src="/images/avatars/default.jpg" style="width:70px; height:70px; border-radius:50%;" alt="Default Avatar">
+    </div>
+    <div class="d-block bg-light" style="flex:1">
+        <div class="border p-3 rounded {{ $limit == 0 ? 'shadow-sm border-info' : '' }}">
+            <p>Comment deleted </p>
+            <p class="border-top pt-1 text-right mb-0">
+                <small class="text-muted">{!! $comment->created_at !!}
+                    @if ($comment->created_at != $comment->deleted_at)
+                        <span class="text-muted border-left mx-1 px-1">(Deleted {!! $comment->deleted_at !!})</span>
+                    @endif
+                </small>
+                <a href="{{ url('comment/') . '/' . $comment->id }}"><i class="fas fa-link ml-1" style="opacity: 50%;"></i></a>
+            </p>
         </div>
     </div>
+
+    {{-- Recursion for children --}}
+    <div class="w-100 mw-100">
+        @php $children = $depth == 0 ? $comment->children->sortByDesc('created_at')->paginate(5) : $comment->children->sortByDesc('created_at') @endphp
+        @foreach ($children as $reply)
+            @php $limit++; @endphp
+
+            @if ($limit >= 5 && $depth >= 1)
+                <a href="{{ url('comment/') . '/' . $comment->id }}"><span class="btn btn-secondary w-100">See More Replies</span></a>
+            @break
+        @endif
+
+        @include('comments._perma_comments', [
+            'comment' => $reply,
+            'reply' => true,
+            'limit' => $limit,
+            'depth' => $depth + 1,
+        ])
+    @endforeach
+    @if ($depth == 0)
+        {!! $children->render() !!}
+    @endif
 </div>
+</div>
+</div>
+@endif
