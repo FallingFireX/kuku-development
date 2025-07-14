3a593a55985e3ab53e8774a5ee648ac5b89476ae
Alliionart <Alliionart@users.noreply.github.com>
2025-07-14 05:36:37 +0000

    refactor: fix blade formatting

--- resources/views/designhub/marking.blade.php
+++ resources/views/designhub/marking.blade.php
@@ -1,25 +1,26 @@
 @extends('layouts.app')
 
 
-@section('title') Design Hub @endsection
+@section('title')
+    Design Hub
+@endsection
 
 
 @section('content')
-{!! breadcrumbs(['Design Hub' => 'Design Hub']) !!}
-<h1>{{ $marking->name }} <span style="text-transform:none;">({{ $marking->recessive }}/{{ $marking->dominant }})</span></h1>
-<p>{{ $marking->short_description }}</p>
+    {!! breadcrumbs(['Design Hub' => 'Design Hub']) !!}
+    <h1>{{ $marking->name }} <span style="text-transform:none;">({{ $marking->recessive }}/{{ $marking->dominant }})</span></h1>
+    <p>{{ $marking->short_description }}</p>
 
-<div class="site-page-content parsed-text">
+    <div class="site-page-content parsed-text">
         {!! $marking->description !!}
     </div>
-
 @endsection
 
 @section('scripts')
     @parent
     <script>
         $(document).ready(function() {
-            
+
         });
     </script>
 @endsection
