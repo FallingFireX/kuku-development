cc0db86d94ec87fe9990677207675ec25a53bbe0
Alliz <58315340+Alliionart@users.noreply.github.com>
2025-08-10 12:56:53 -0600

    Site Options Mini
    + Add Site Options table (not to be confused with site settings)
    + Create Service for options
    + Create model for options

---
+++ app/Models/SiteOptions.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+class SiteOptions extends Model {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'key', 'value',
+    ];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'site_options';
+    /**
+     * Validation rules for creation.
+     *
+     * @var array
+     */
+    public static $createRules = [
+        'key'        => 'required|unique:site_options|between:3,100',
+        'value'       => 'nullable|between:0,2048',
+    ];
+
+    /**
+     * Validation rules for updating.
+     *
+     * @var array
+     */
+    public static $updateRules = [
+        'key'        => 'required|unique:site_options|between:3,100',
+        'value'       => 'nullable|between:0,2048',
+    ];
+
+    /**********************************************************************************************
+
+        ACCESSORS
+
+    **********************************************************************************************/
+
+    /**
+     * Gets the power required to edit this model.
+     *
+     * @return string
+     */
+    public function getAdminPowerAttribute() {
+        return 'edit_data';
+    }
+}
---
+++ app/Services/SiteOptionsManager.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\SiteOptions;
+use Illuminate\Support\Facades\DB;
+
+class SiteOptionsManager extends Service {
+    /*
+    |--------------------------------------------------------------------------
+    | Site Options Manager
+    |--------------------------------------------------------------------------
+    |
+    | Handles creation, modification and usage of site options.
+    |
+    */
+
+    /**
+     * Create an option.
+     *
+     * @param array                 $data
+     * @param \App\Models\SiteOptions $option
+     *
+     * @return \App\Models\SiteOptions|bool
+     */
+    public function createOption($data) {
+        DB::beginTransaction();
+
+        try {
+            if (!isset($data['key'])) {
+                throw new \Exception('Invalid key, cannot create.');
+            }
+
+            $option = SiteOptions::create([
+                'key'        => $data['key'],
+                'value'      => $data['option'] ?? null,
+            ]);
+
+            return $this->commitReturn($option);
+        } catch (\Exception $e) {
+            $this->setError('error', $e->getMessage());
+        }
+
+        return $this->rollbackReturn(false);
+    }
+
+    /**
+     * Update or create an option.
+     *
+     * @param array                 $data
+     *
+     * @return \App\Models\SiteOptions|bool
+     */
+    public function updateOption($data) {
+        DB::beginTransaction();
+
+        try {
+            if (!isset($data['key'])) {
+                throw new \Exception('Invalid option selected.');
+                $this->createOption($data);
+            }
+
+            $option = SiteOptions::where('key', $data['key'])->first();
+
+            $option->update([
+                'value'      => $data['option'] ?? null,
+            ]);
+
+            return $this->commitReturn($option);
+        } catch (\Exception $e) {
+            $this->setError('error', $e->getMessage());
+        }
+
+        return $this->rollbackReturn(false);
+    }
+
+    /**
+     * Delete an option.
+     *
+     * @param array                 $data
+     *
+     * @return bool
+     */
+    public function deleteOption($data) {
+        DB::beginTransaction();
+
+        try {
+            if (!isset($data['key'])) {
+                throw new \Exception('Invalid option selected.');
+            }
+            $option = SiteOptions::where('key', $data['key'])->first();
+
+            $option->delete();
+
+            return $this->commitReturn(true);
+        } catch (\Exception $e) {
+            $this->setError('error', $e->getMessage());
+        }
+
+        return $this->rollbackReturn(false);
+    }
+}
