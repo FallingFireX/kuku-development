d9e4c96390a5e7cfac790354677054630856eb60
Sydney Hurst <evesperson@gmail.com>
2025-09-06 06:49:49 -0600

    credits fix

--- config/lorekeeper/ext-tracker/teams.php
+++ config/lorekeeper/ext-tracker/teams.php
@@ -3,7 +3,7 @@
 return [
     'wiki_key' => 'Teams',
     'creators' => json_encode([
-        'Uri' => 'https://github.com/fallingfirex/',
+        'FallingFireX' => 'https://github.com/fallingfirex/',
     ]),
     'version'  => '1.0.0',
 ];
