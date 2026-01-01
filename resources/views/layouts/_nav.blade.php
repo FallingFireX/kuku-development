b8aabd3d967c582b7d279c7d14c2634e4aa189c6
Sydney Hurst <evesperson@gmail.com>
2026-01-01 01:40:55 -0700

    ?

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -180,7 +180,7 @@
                         <a class="dropdown-item" href="{{ url('info/coliseum') }}">
                             Coliseum
                         </a>
-                        <a class="dropdown-item" href="https://kukuri-arpg.w3spaces.com/activities/traveling-merchant.html">
+                        <a class="dropdown-item" href="https://www.kukuri-arpg.com/info/merchant">
                             Traveling Merchant
                         </a>
                         <div class="dropdown-divider"></div>
