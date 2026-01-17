7180f37ddcb58b99570b8a63e9b868f55c37fe94
Sydney Hurst <evesperson@gmail.com>
2026-01-16 17:26:38 -0700

    fix various minor errors

--- resources/views/home/_submission_form.blade.php
+++ resources/views/home/_submission_form.blade.php
@@ -57,7 +57,7 @@
 
 
 <div class="form-group">
-    {!! Form::label('comments', 'Comments (Optional)') !!} {!! add_help('Enter a comment for your ' . ($isClaim ? 'claim' : 'submission') . ' (no HTML). This will be viewed by the mods when reviewing your ' . ($isClaim ? 'claim' : 'submission') . '.') !!}
+    {!! Form::label('comments', 'Comments (Optional)') !!} {!! add_help('Enter a comment for your ' . ($isClaim ? 'claim' : 'submission') . '. This will be viewed by the mods when reviewing your ' . ($isClaim ? 'claim' : 'submission') . '.') !!}
     {!! Form::textarea('comments', isset($submission->comments) ? $submission->comments : old('comments') ?? Request::get('comments'), ['class' => 'form-control wysiwyg']) !!}
 </div>
 
@@ -227,3 +227,7 @@
 @else
     @include('widgets._loot_select_row', ['items' => $items, 'currencies' => $currencies, 'showLootTables' => false, 'showRaffles' => false])
 @endif
+
+@section ('scripts')
+@include('js._tinymce_wysiwyg')
+@endsection
--- resources/views/home/characters.blade.php
+++ resources/views/home/characters.blade.php
@@ -51,6 +51,13 @@
     {!! Form::submit('Save Order', ['class' => 'btn btn-primary']) !!}
 {!! Form::close() !!}
 
+<p>You can select one of your characters to be featured on your profile here.</p>
+{!! Form::open(['url' => 'characters/select-character']) !!}
+    {!! Form::select('character_id', $characters->pluck('fullName', 'id'), Auth::user()->settings->selected_character_id, ['class' => 'form-control mb-2 default character-select', 'placeholder' => 'Select Character']) !!}
+    <div class="text-right">
+        {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
+    </div>
+{!! Form::close() !!}
 
 @endsection
 @section('scripts')
